a8555afde4dac69616f21f672fc0d13b19ed764c
mrmuhammadrifki <immuhammadrifki@gmail.com>
2023-07-08 01:06:17 +0700

    finished open music app version 1

---
+++ src/api/albums/handler.js
@@ -0,0 +1,68 @@
+const autoBind = require('auto-bind');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+
+    const albumId = await this._service.addAlbum(request.payload);
+
+    const response = h.response({
+      status: 'success',
+      data: {
+        albumId,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+
+    const album = await this._service.getAlbumById(id);
+    const songs = await this._service.getSongsByAlbumId(id);
+
+    return {
+      status: 'success',
+      data: {
+        album: {
+          ...album,
+          songs,
+        },
+      },
+    };
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+
+    const { id } = request.params;
+
+    await this._service.editAlbumById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Album has been updated!',
+    };
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+
+    await this._service.deleteAlbumById(id);
+
+    return {
+      status: 'success',
+      message: 'Album has been deleted!',
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+const mapDBToModel = ({ id, title, year, genre, performer, duration, album_id }) => ({
+  id,
+  title,
+  year,
+  genre,
+  performer,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = mapDBToModel;
